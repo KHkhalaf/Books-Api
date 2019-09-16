591dab13ddaccede467ff3c0fb4eaa881b9ef336
hp <hp@DESKTOP-FKTABFC>
2019-09-16 23:27:00 +0300

    a Commit

---
+++ Models/BooksDBContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace BooksAPI.Models
+{
+    public partial class BooksDBContext : DbContext
+    {
+        public BooksDBContext()
+        {
+        }
+
+        public BooksDBContext(DbContextOptions<BooksDBContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Book> Book { get; set; }
+
+    }
+}
